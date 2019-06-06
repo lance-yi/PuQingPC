527683c5ce3504f2b13d83f0fa5630a085b63ea6
bonjour520 <l>
2019-06-06 19:31:25 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//滚动检测
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+        $(".main-header").css({
+            "position": "fixed",
+            "top": 0,
+            "z-index": 1030,
+            "background": "#fff",
+            "box-shadow": "0 6px 6px 0 rgba(232,237,250,.6)"
+        });
+        $(".top-bar").css("display", "none");
+    } else {
+        $(".main-header").removeClass("active");
+        $(".top-bar").css("display", "block");
+        $(".main-header").css("position", "relative");
+    }
+});
